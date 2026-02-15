0c66f7879289dd3ee785483d56e7ce363007f40d
Václav Jež <jez.vasek@gmail.com>
2026-02-15 13:19:40 +0100

    add DS_store ignore to mini.files

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -2,10 +2,19 @@ return {
     "echasnovski/mini.files",
     version = false,
     config = function()
+        local ignore_map = {
+            ['.DS_Store'] = true,
+        }
+
         require("mini.files").setup({
             windows = {
                 preview = true,
                 width_preview = 55,
+            },
+            content = {
+                filter = function(fs_entry)
+                    return not ignore_map[fs_entry.name]
+                end,
             },
         })
     end,
